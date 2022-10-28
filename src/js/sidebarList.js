39a695661d25c57ccb5b6206de8cb8def1873099
Anton <a.chertok@akatech.com.ua>
2022-10-28 22:23:28 +0300

    sidebar-current

--- src/js/sidebarList.js
+++ src/js/sidebarList.js
@@ -4,3 +4,16 @@ import template from '../templates/sidebar-tmpl.hbs';
 const sidebarList = document.querySelector('.js-tree-list');
 
 sidebarList.innerHTML = template(mainPageProductsList);
+
+sidebarList.addEventListener('click', e => {
+  const ref = e.target;
+  if (!ref.classList.contains('tree-list__link')) {
+    return;
+  }
+
+  const prevCurrentRef = document.querySelector('.tree-list__link--current');
+  if (prevCurrentRef) {
+    prevCurrentRef.classList.remove('tree-list__link--current');
+  }
+  ref.classList.add('tree-list__link--current');
+});
